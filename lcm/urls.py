0a9d275d956fde9d9d277b392035d5dacf525dac
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2014-08-06 23:43:55 +0530

    created project structure

---
+++ lcm/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('lifecodemohit.views',
+    # Examples:
+    # url(r'^$', 'lcm.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'home'),
+)
